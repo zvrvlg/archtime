9bdd42e0a330fb5b197e31e3f5b263b068623869
zvrvlg <vaol3007@yandex.ru>
2026-08-14 10:13:36 +0300

    предверсия

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'navigator-v1';
+const FILES = [
+  '/',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
